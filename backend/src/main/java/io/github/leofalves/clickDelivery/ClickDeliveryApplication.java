dc9d22ea3ab5bff0445dc27e2529d7318771c0e2
mtzcpd1842 <leoalves@cvccorp.com.br>
2021-01-04 19:34:47 -0300

    Project created by Spring Initializr

---
+++ backend/src/main/java/io/github/leofalves/clickDelivery/ClickDeliveryApplication.java
@@ -0,0 +1,13 @@
+package io.github.leofalves.clickDelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClickDeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClickDeliveryApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/io/github/leofalves/clickDelivery/ClickDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.leofalves.clickDelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClickDeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
