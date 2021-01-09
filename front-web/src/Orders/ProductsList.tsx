1ac83535e9efcc210e813514ff9000eb952bfcf5
Leo Fabiano Alves <leo.fabiano@gmail.com>
2021-01-09 10:48:34 -0300

    API fetch and ProductCard

---
+++ front-web/src/Orders/ProductsList.tsx
@@ -0,0 +1,20 @@
+import ProductCard from "./ProductCard";
+import { Product } from "./types";
+
+type Props = {
+    products : Product[];
+}
+
+function ProductsList({products} : Props) {
+    return (
+        <div className="orders-list-container">
+            <div className="orders-list-items">
+                {products.map(item => (
+                    <ProductCard key={item.id} product={item} />
+                ))}
+            </div>
+        </div>
+    )
+};
+
+export default ProductsList;
